c4828faeaf1fca3b63b2a7d4a5c5aef33a78f06e
MohammadAli Karimi <ma.karimi76@gmail.com>
2021-08-09 23:48:49 +0430

    create categories api

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'components/Head'
 import ShopLayout from 'layouts/Shop.layout'
 
-export default function Home() {
+import { getCategories } from 'api/categories'
+
+export default function Home({ categories }) {
+  console.log(categories)
   return (
     <>
       <Head title="صفحه اصلی" />
@@ -10,4 +13,13 @@ export default function Home() {
       </ShopLayout>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const categories = await getCategories()
+
+  return {
+    props: { categories },
+    revalidate: 1,
+  }
 }
